cc361a6dd88da675af8cf7882fa3c98a9c8eb946
hoehoeabi <0203jyh00@naver.com>
2026-04-07 17:52:40 +0900

    feat: id를 기반으로 찾아야 하니 리스트보다 맵이 어울릴거같아서 수정 및 기능 추가

--- src/main/java/domain/Articles.java
+++ src/main/java/domain/Articles.java
@@ -1,21 +1,36 @@
 package domain;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Articles {
-    private final List<Article> list = new ArrayList<>();
+    private final Map<Integer, Article> articleMap = new HashMap<>();
     private int lastId = 0;
 
     public int write(String title, String content) {
         int id = ++lastId;
         LocalDateTime now = LocalDateTime.now();
-        list.add(new Article(id, title, content, now , now));
+        articleMap.put(id, new Article(id, title, content, now, now));
         return id;
     }
 
     public List<Article> findAll() {
+        List<Article> list = new ArrayList<>(articleMap.values());
+        list.sort((a1, a2) -> a2.id - a1.id);
         return list;
+    }
+
+    public Article findById(int id) {
+        return articleMap.get(id);
+    }
+
+    public boolean deleteById(int id) {
+        return articleMap.remove(id) != null;
+    }
+
+    public void update(Article article, String title, String content) {
+        article.title = title;
+        article.content = content;
+        article.modDate = LocalDateTime.now();
     }
 }
